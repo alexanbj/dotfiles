a959b1460ebcef52e74b589fffa865482e71d84a
Alexander Bjerkan <alb@knowit.no>
2017-01-03 09:33:25 +0100

    Set Atom as editor

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,6 +1,7 @@
 # Sets reasonable macOS defaults
 #
 # Or, in other words, set shit how I like in macOS.
+# Inspiration: https://github.com/mathiasbynens/dotfiles
 #
 # Run ./set-defaults.sh and you'll be good to go!
 
@@ -8,8 +9,19 @@
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+# Set a really fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+# Finder
 # Always open everything in Finder's list view. This is important.
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
-# Set a really fast key repeat.
-defaults write NSGlobalDomain KeyRepeat -int 1
+# Show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
